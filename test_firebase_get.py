d4a057f73b07c552e3d08af15b4f80184902ba6a
Yoshida Rick <chikara.f.yoshida@gmail.com>
2019-11-19 12:45:21 +0900

    gpe_getter_raspberryを追加 sqlを使わずログを取ってfirebaseに上げることにする どうやら認証に時間がかかるだけのようで、GPSを取ることにはそんなに時間かからないっぽい。割と正確に距離測れそう。

--- test_firebase_get.py
+++ test_firebase_get.py
@@ -2,5 +2,6 @@ from firebase import firebase
 import requests
 
 firebase = firebase.FirebaseApplication('https://drive-manager-18863.firebaseio.com/', None)
-print(firebase.get('/drive-manager-18863/', ''))
-
+all_firevase_data = firebase.get('/drive-manager-18863/', '')
+print(type(all_firevase_data))
+print(all_firevase_data['2019-11-18 '])
